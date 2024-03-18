7dbf18d9fa2ef44834f6dc43d84bc0158f26bcc3
blackblitz <williamzhu345@gmail.com>
2024-03-18 20:49:56 +0800

    Load data only once for batch gradient descent

--- train/joint.py
+++ train/joint.py
@@ -18,10 +18,17 @@ def joint(
     for i, dataset in enumerate(dataseq.train()):
         concat = dataset if i == 0 else ConcatDataset([concat, dataset])
         if dataloader_kwargs is None:
-            kwargs = {'batch_size': len(concat), 'collate_fn': numpy_collate}
-        else:
-            kwargs = dataloader_kwargs
-        for _ in range(nepochs):
-            for x, y in DataLoader(concat, **kwargs):
+            x, y = next(iter(
+                DataLoader(
+                    concat,
+                    batch_size=len(concat),
+                    collate_fn=numpy_collate
+                )
+            ))
+            for _ in range(nepochs):
                 state = step(state, x, y)
+        else:
+            for _ in range(nepochs):
+                for x, y in DataLoader(concat, **dataloader_kwargs):
+                    state = step(state, x, y)
         yield state, loss
