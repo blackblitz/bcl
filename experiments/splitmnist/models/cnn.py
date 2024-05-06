b61f18a2e91fc96eebeae86bbb9493cd56be6a85
blackblitz <williamzhu345@gmail.com>
2024-05-06 14:49:07 +0800

    Update script for Split MNIST

--- experiments/splitmnist/models/cnn.py
+++ experiments/splitmnist/models/cnn.py
@@ -19,9 +19,9 @@ class Main(nn.Module):
 class Consolidator(nn.Module):
     @nn.compact
     def __call__(self, x):
-        x = nn.Dense(50)(x)
+        x = nn.Dense(200)(x)
         x = nn.swish(x)
-        x = nn.Dense(50)(x)
+        x = nn.Dense(200)(x)
         x = nn.swish(x)
         x = nn.Dense(1)(x)
         return x
